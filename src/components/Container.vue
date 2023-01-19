d144a05b1e23bf860825977eec3e89b4cdb0d4e8
Sunwoo <dlatjsdn205@Naver.com>
2023-01-19 20:42:21 +0900

    [ADD]add Container,Post components

---
+++ src/components/Container.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Post v-for="(data, i) in postData" :key="i" :postData="data"></Post>
+  </div>
+</template>
+
+<script>
+import Post from "./Post.vue";
+export default {
+  components: {
+    Post,
+  },
+  data() {
+    return {
+      postData: this.postData,
+    };
+  },
+  props: {
+    postData: Array,
+  },
+};
+</script>
+
+<style></style>
